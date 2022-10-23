2ed91fde02b7dce779ea7cca42b3bd9c8dff1351
Soham Sawant <72149891+sohamsawant02@users.noreply.github.com>
2022-10-24 00:04:12 +0530

    Add files via upload

---
+++ tree_traversal.py
@@ -0,0 +1,50 @@
+# Tree traversal in Python
+
+
+class Node:
+    def __init__(self, item):
+        self.left = None
+        self.right = None
+        self.val = item
+
+
+def postorder(root):
+
+    if root:
+        # Traverse left
+        postorder(root.left)
+        # Traverse right
+        postorder(root.right)
+        # Traverse root
+        print(str(root.val) + "->", end='')
+
+
+def preorder(root):
+
+    if root:
+        # Traverse root
+        print(str(root.val) + "->", end='')
+        # Traverse left
+        preorder(root.left)
+        # Traverse right
+        preorder(root.right)
+
+
+root = Node("2")
+root.left = Node("7")
+root.left.left = Node("2")
+
+root.left.right=Node("6")
+root.left.right.left=Node("5")
+root.left.right.right=Node("11")
+
+root.right = Node("5")
+root.right.right = Node("9")
+root.right.right.left = Node("4")
+
+
+print("\nPreorder traversal ")
+preorder(root)
+
+print("\nPostorder traversal ")
+postorder(root)
